40e4a5bb03159673dcabbafd6061b3319b5fdbb9
simonlundmark1 <155955074+simonlundmark1@users.noreply.github.com>
2024-09-18 13:31:13 +0200

    heaejkd

--- api/products.js
+++ api/products.js
@@ -1,23 +1,24 @@
 // /api/products.js
 const mongoose = require('mongoose');
-const Product = require('../graphql/models/Product'); // Adjust the path as necessary
+const Product = require('../models/Product'); // Adjust the path as necessary
 
-const uri = process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/products`;
+const uri =
+  process.env.MONGODB_URI ||
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/products`;
 
 let cachedDb = null;
 
 async function connectToDatabase() {
   if (cachedDb && mongoose.connection.readyState === 1) {
-    // Use existing database connection
     return cachedDb;
   }
 
-  // Create a new database connection
   try {
-    cachedDb = await mongoose.connect(uri, {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    cachedDb = mongoose.connection;
     console.log('Connected to MongoDB');
     return cachedDb;
   } catch (err) {
@@ -28,12 +29,12 @@ async function connectToDatabase() {
 
 module.exports = async (req, res) => {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://ims-one-theta.vercel.app/'); // Replace '*' with your frontend domain in production
+  res.setHeader('Access-Control-Allow-Origin', 'https://ims-one-theta.vercel.app'); // Use your actual frontend domain
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end(); // Respond to preflight requests
+    res.status(200).end();
     return;
   }
 
